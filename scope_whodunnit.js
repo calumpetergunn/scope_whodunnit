23c322b92e047669722bf2b86810bf9a0ac94057
calumpetergunn <calumgunn@gmail.com>
2021-03-29 17:12:19 +0100

    initial commit

---
+++ scope_whodunnit.js
@@ -0,0 +1,243 @@
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Library',
+//     weapon: 'Rope'
+//   };
+  
+//   const declareMurderer = function() {
+//     return `The murderer is ${scenario.murderer}.`;
+//   }
+  
+// //   I think it'll be Miss Scarlet
+//   const verdict = declareMurderer();
+// //   console.log(verdict);
+// // yay
+
+
+
+
+
+// const murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// I think it will be Professor Plum - the changeMurdered function won't have changed it - basically, 'assignment to constant variable not allowed, as predicted'
+
+
+
+
+
+// let murderer = 'Professor Plum';
+
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+
+// // first verdict - Mrs Peacock
+// // second verdict - Prof Plum
+// woop woop
+
+
+
+
+
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+
+// SUSPECT THREE WILL BE Colonel Mustard in the list of suspects, but Mrs peacock when suspectThree is called
+// I AM A GOLDEN GOD
+
+
+
+
+
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Kitchen',
+//     weapon: 'Candle Stick'
+//   };
+  
+//   const changeWeapon = function(newWeapon) {
+//     scenario.weapon = newWeapon;
+//   }
+  
+//   const declareWeapon = function() {
+//     return `The weapon is the ${scenario.weapon}.`;
+//   }
+  
+//   changeWeapon('Revolver');
+//   const verdict = declareWeapon();
+//   console.log(verdict);
+
+//   VERDICT WILL BE 'The weapon is the Revolver
+// I should be teaching this course
+
+
+
+
+// let murderer = 'Colonel Mustard';
+
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+
+//   const plotTwist = function() {
+//     murderer = 'Mrs. White';
+//   }
+
+//   plotTwist();
+// }
+
+// const declareMurderer = function () {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// 'The murdered is Colonel Mustard' - Woops
+
+
+
+
+
+// let murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
+
+//     unexpectedOutcome();
+//   }
+
+//   plotTwist();
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// // The murderer is Miss Scarlett booooooooo
+
+
+
+
+// const scenario = {
+//     murderer: 'Mrs. Peacock',
+//     room: 'Conservatory',
+//     weapon: 'Lead Pipe'
+//   };
+  
+//   const changeScenario = function() {
+//     scenario.murderer = 'Mrs. Peacock';
+//     scenario.room = 'Dining Room';
+  
+//     const plotTwist = function(room) {
+//       if (scenario.room === room) {
+//         scenario.murderer = 'Colonel Mustard';
+//       }
+  
+//       const unexpectedOutcome = function(murderer) {
+//         if (scenario.murderer === murderer) {
+//           scenario.weapon = 'Candle Stick';
+//         }
+//       }
+  
+//       unexpectedOutcome('Colonel Mustard');
+//     }
+  
+//     plotTwist('Dining Room');
+//   }
+  
+//   const declareWeapon = function() {
+//     return `The weapon is ${scenario.weapon}.`
+//   }
+  
+//   changeScenario();
+//   const verdict = declareWeapon();
+//   console.log(verdict);
+
+//   The Weapon is Candlestick
+// my dignity has been re-affirmed
+
+
+
+
+
+// let murderer = 'Professor Plum';
+
+// if (murderer === 'Professor Plum') {
+//   let murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+
+// 'The murderer is Prof Plum'
+// I have clearly missed my calling
+
+
+const scenario = {
+        murderer: 'Miss Scarlet',
+        room: 'Kitchen',
+        weapon: 'Candle Stick'
+      };
+
+const changeMurderer = function() {
+    if (scenario.weapon === "Rope"){
+    scenario.murderer = "Mrs. Peacock";
+    } else {
+        scenario.murderer = "Colonel Mustard";
+        scenario.room = "Hot Tub ;)"
+    }
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer} and they did it in the ${scenario.room}`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
